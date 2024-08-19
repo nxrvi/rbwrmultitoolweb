87e94148e0962dc2aaa54b277ef3da8f7b8aa680
Nxrvi <teamnervi@gmail.com>
2024-08-19 13:48:25 +0200

    Changes a lot of changes :O
    Points Calculator added
    Lots of other stuff
    and stuff

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'img', to: 'img' },
+        { from: 'css', to: 'css' },
+        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'robots.txt', to: 'robots.txt' },
+        { from: '404.html', to: '404.html' },
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: 'rbwricon.png', to: 'rbwricon.png' },
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'rbwricon.svg', to: 'rbwricon.svg' },
+      ],
+    }),
+  ],
+});
