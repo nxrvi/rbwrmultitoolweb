5e461ae4abed04df3830a8a9210d3ff8d0abe757
Cosmos255 <150472944+Cosmos255@users.noreply.github.com>
2024-11-01 14:49:19 +0200

    Add files via upload

--- js/resources.js
+++ js/resources.js
@@ -6,16 +6,24 @@ let navbar = document.getElementById("idkfix");
 let overlayfix = document.getElementById("idkfix2");
 
 
-if(navbar && overlayfix){
-    let height = navbar.clientHeight;
-    
-    overlayfix.style.marginTop = `${height}px`;
 
-    console.log("It worked");
+function updateFix(){
+    if(navbar && overlayfix){
+        let height = navbar.clientHeight;
+        
+        overlayfix.style.marginTop = `${height}px`;
     
-}else{
-    console.log("Not Found");
+        
+    }
 }
+
+
+window.addEventListener('resize', updateFix);
+
+
+updateFix();
+
+
 
 
 
